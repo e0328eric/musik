ce8d62847a54254dcffc7b4bdace32365d4e2078
Sungbae Jeong <almagest0328@gmail.com>
2025-06-07 02:15:03 +0900

    change codebase back to zig

--- src/render.h
+++
@@ -1,8 +0,0 @@
-#ifndef MUSIK_RENDER_H_
-#define MUSIK_RENDER_H_
-
-#include "terminal.h"
-
-bool drawMusik(Terminal* term, double total_music_len, double curr_music_pos);
-
-#endif // MUSIK_RENDER_H_
--- src/utility.h
+++
@@ -1,8 +0,0 @@
-#ifndef MUSIK_UTILITY_H_
-#define MUSIK_UTILITY_H_
-
-#include <stdint.h>
-
-void musikSleep(uint32_t millisecs);
-
-#endif // MUSIK_UTILITY_H_
