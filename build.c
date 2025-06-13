2c69a29cb7e41906da69c4235816789d0c5195b2
Sungbae Jeong <almagest0328@gmail.com>
2025-06-13 12:33:01 +0900

    link with libc and use nob.h

--- build.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-#ifdef _WIN32
-    system("fasm ./src/Windows/musik.asm musik.exe");
-#endif
-}
--- src/Windows/musik.asm
+++ src/Windows/musik.asm
@@ -1,9 +1,12 @@
-format PE64 console
-entry _start
+format MS64 COFF
+public main
 
-include "win64w.inc"
+include "macro/struct.inc"
 include "encoding/utf8.inc" ; `du` converts utf8 into windows wchar_t type
+include "winapi.inc"
+include "libc.inc"
 
+include "structs.inc"
 include "utility.inc"
 include "terminal.inc"
 
@@ -23,7 +26,7 @@ include "terminal.inc"
 
 section ".text" code readable executable
 
-_start:
+main:
     ;; at least 32 bytes of stack must be allocated even if the function
     ;; does not use more than four arguments
     sub  rsp, 28h
@@ -60,36 +63,23 @@ _start:
 
 invalid_argument:
     invoke MessageBoxW, 0, invalid_argument_msg, msg_title, MB_OK or MB_ICONERROR
-    lea rdi, [_usage]
-    call printStrANul
+    lea rcx, [usage]
+    call printf
     jmp exit_failure
 
 exit_failure:
     invoke ExitProcess, 1
 
-section ".data" data readable
+section ".mdata" data readable
 msg_title            du "Musik Error", 0
 invalid_argument_msg du "Invalid argument was given. See the console for more "
                      du "information.", 0
 term_err_str         du "Terminal handling failed.", 0
 
-_usage db 0dh, 0ah
-       db "usage: musik [-L] <wav filename>", 0dh, 0ah, 0
+usage db "usage: musik [-L] <wav filename>", 10, 0
 
-section ".bss" data readable writable
+section ".mglobal" data readable writable
 argc     dd ?
 argv     dq ?
 filename dq ?
-
-section ".idata" import data readable writable
-library kernel32, "KERNEL32.DLL",\
-        user32,   "USER32.DLL",\
-        shell32,  "SHELL32.DLL",\
-        winmm,    "WINMM.DLL"
-
-include "api/kernel32.inc"
-include "api/user32.inc"
-
-import shell32,\
-       CommandLineToArgvW, "CommandLineToArgvW"
 
